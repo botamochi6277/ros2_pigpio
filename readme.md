8e0bc3a3614474a6b29ff3c0482dd2163c474ec2
botamochi6277 <botamochi6277@icloud.com>
2020-10-29 21:45:42 +0900

    add pwm writer

--- readme.md
+++ readme.md
@@ -1,11 +1,12 @@
 # ROS2 PIGPIO
 
 A ROS2 package to use [pigpio](http://abyz.me.uk/rpi/pigpio/).
+This package is tested with Raspberry Pi 4 Model B with Ubuntu 20.04 LTS and foxy.
 
 ## Install
 
 1. Install pigpio following official instruction
-1. Download this repository in your ros2 workspace
+1. Download this repository in `<your ros2 workspace>/src`
 1. build this package
 
 
@@ -35,4 +36,19 @@ ros2 topic pub --once gpio_output_21 std_msgs/msg/Bool "{data:{true}}"
 Write low signal:
 ```
 ros2 topic pub --once gpio_output_21 std_msgs/msg/Bool "{data:{}}"
+```
+
+## Write PWM Signals
+
+To write pwm signal with GPIO-18, run a following command. 
+
+```
+ros2 run ros2_pigpio gpio_pwm_writer  --ros-args --param pin:=18
+```
+
+A duty cycle is 0--255 which corresponded with 0%--100%
+Write 128 (50%) pwm signal:
+
+```
+ros2 topic pub --once gpio_pwm_18 std_msgs/msg/Int16 '{data: 128}'
 ```
---
+++ src/gpio_pwm_writer.cpp
@@ -0,0 +1,65 @@
+/**
+ * @file gpio_writer.cpp
+ *
+ * @brief ROS2 Listener to output pwm signals.
+**/
+#include <chrono>
+#include <memory>
+#include <thread>
+#include <sstream>
+#include <iomanip>
+
+#include <pigpiod_if2.h>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int16.hpp"
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+class PWMWriter : public rclcpp::Node
+{
+private:
+  int pi_;
+  int pin_;
+  rclcpp::Subscription<std_msgs::msg::Int16>::SharedPtr subscription_;
+  void topic_callback(const std_msgs::msg::Int16::SharedPtr msg) const
+  {
+    set_PWM_dutycycle(pi_, pin_,  msg->data);
+    RCLCPP_INFO(this->get_logger(), "Write %03d on GPIO-%d", msg->data, pin_);
+  }
+
+public:
+  PWMWriter() : Node("gpio_pwm_subscriber")
+  {
+    this->declare_parameter<int>("pin", 17);
+    this->get_parameter("pin", pin_);
+    RCLCPP_INFO(this->get_logger(), "Write GPIO-%02d", pin_);
+    pi_ = pigpio_start(NULL, NULL);
+
+    if (pi_ >= 0)
+    {
+      set_mode(pi_, pin_, PI_OUTPUT);
+      std::stringstream ss;
+      ss << "gpio_pwm_" << std::setw(2) << pin_;
+      subscription_ = this->create_subscription<std_msgs::msg::Int16>(
+          ss.str(), 10, std::bind(&PWMWriter::topic_callback, this, _1));
+    }
+    else
+    {
+      RCLCPP_ERROR(this->get_logger(), "cannot connect pigpiod");
+    }
+  }
+  ~PWMWriter()
+  {
+    set_mode(pi_, pin_, PI_INPUT);
+    pigpio_stop(pi_);
+  }
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PWMWriter>());
+  rclcpp::shutdown();
+  return 0;
+}
